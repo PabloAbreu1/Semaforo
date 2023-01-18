3779f7e5fc9d810a62f97733cf700d11b45a1cfd
PabloAbreu1 <pagustin2404@gmail.com>
2023-01-18 13:35:46 +0000

    semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+   const [color1,setColor1]= useState("");
+   const [color2,setColor2]= useState("");
+   const [color3,setColor3]= useState("");
+
+	function encenderRojo() {
+
+		if(color1 === "btn-danger"){
+			setColor1("")
+		}else{
+		setColor1("btn-danger")
+		setColor2("")
+		setColor3("")
+	}}
+	function encenderAmarillo() {
+
+		if(color2 === "btn-warning"){
+			setColor2("")
+		}else{
+		setColor2("btn-warning")
+		setColor1("")
+		setColor3("")
+	}}
+	function encenderVerde() {
+
+		if(color3 === "btn-success"){
+			setColor3("")
+		}else{
+		setColor3("btn-success")
+		setColor2("")
+		setColor1("")
+		
+	}}
+
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<>
+
+	<div className="">
+		<div className="text-center d-grid gap-2 col-1 mx-auto bg-black btn-group-vertical">
+			<button className={"shadow-lg p-3 mb-5 btn-lg rounded-circle btn "+color1} onClick={encenderRojo}>Rojo</button>
+			<button className={"shadow-lg p-3 mb-5 btn-lg rounded-circle btn "+color2} onClick={encenderAmarillo}>Amarillo</button>
+			<button className={"shadow-lg p-3 mb-5 btn-lg rounded-circle btn "+color3} onClick={encenderVerde}>Verde</button>
 		</div>
+	</div>
+
+	</>
 	);
+
 };
 
 export default Home;
